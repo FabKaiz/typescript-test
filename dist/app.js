98d668df8046bc4f8a9e92dd6c920d22c44de322
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-03 12:26:19 +0100

    compteur et type

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,23 @@
-console.log('hello world');
+"use strict";
+const a = 'hello';
+const n = 3;
+const b = true;
+const d = null;
+const arr = ['aze', 'azee', 3];
+const user1 = { firstname: 'Fab', lastname: 'Kaiz' };
+//or
+const user2 = { firstname: 'Fab', lastname: 'Kaiz' };
+const date = new Date();
+const cb = (e) => {
+};
 const compteur = document.querySelector('#compteur');
 let i = 0;
 const increment = (e) => {
+    e.preventDefault;
     i++;
-    compteur.querySelector('span').innerText = i.toString();
+    const span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector('span');
+    if (span) {
+        span.innerText = i.toString();
+    }
 };
-compteur.addEventListener('click', increment);
+compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
-console.log('hello world');
+const a: string = 'hello'
+const n: number = 3
+const b: boolean = true
+const d: null = null
+const arr: any[] = ['aze', 'azee', 3]
+const user1: {firstname: string, [key: string]: string} = {firstname: 'Fab', lastname: 'Kaiz'}
+//or
+const user2 = {firstname: 'Fab', lastname: 'Kaiz'}
+const date: Date = new Date()
+const cb: Function = (e: MouseEvent): void => { // Function ou l'on nutilise pas le return
 
-const compteur =document.querySelector('#compteur');
-let i = 0;
-
-const increment = (e) => {
-  i++;
-  compteur.querySelector('span').innerText = i.toString();
 }
 
-compteur.addEventListener('click', increment);
+const compteur = document.querySelector('#compteur');
+let i = 0;
+
+const increment = (e: Event) => {
+  e.preventDefault
+  i++;
+  const span = compteur?.querySelector('span')
+  if (span) {
+    span.innerText = i.toString();
+  }
+}
+
+compteur?.addEventListener('click', increment);
