f5d44de6bd6e1b3c15757a0de2dc867196a43ba8
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-03 12:10:20 +0100

    ts config  setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
 console.log('hello world');
+
+const compteur =document.querySelector('#compteur');
+let i = 0;
+
+const increment = (e) => {
+  i++;
+  compteur.querySelector('span').innerText = i.toString();
+}
+
+compteur.addEventListener('click', increment);
