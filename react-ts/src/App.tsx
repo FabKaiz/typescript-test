d10ecd92fcd046625bbb645f63e5fb8aaca05b09
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-07 12:01:39 +0100

    input field setup

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header'
 import InputField from './components/InputField/InputField';
 
 import './App.css';
+import { Todo } from './model';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('')
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (todos) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+      setTodo('')
+    }
+  }
+
+  console.log(todos);
+
   return (
     <div className="App">
-      <Header title="Taskify" color='#1CE' />
-      <InputField />
+      <Header title="Taskify" color="#1CE" />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <TodoLsit /> */}
+
+      {todos.map((todo) => (
+        <li>{todo.todo}</li>
+      ))}
     </div>
   );
 }
--- react-ts/src/components/InputField/InputField.tsx
+++ react-ts/src/components/InputField/InputField.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './InputField.css'
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  
   return (
-    <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
-      <button className="input__submit" type="submit">Add</button>
+    <form className="input" onSubmit={(e) => {
+      handleAdd(e)
+      inputRef.current?.blur()
+    }}>
+      <input
+        ref={inputRef}
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+      />
+      <button className="input__submit" type="submit">
+        Add
+      </button>
     </form>
-  )
+  );
 }
 
 export default InputField
